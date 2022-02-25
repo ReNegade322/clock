ab3d9aa7b39fc6d8df9faa9d0a42db084d07c7da
ReNegade <evis2k@mail.ru>
2022-02-25 21:43:52 +0300

    change color for each second

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ window.onload = function() {
       let seconds = new Date().getSeconds();
       document.getElementById("seconds").innerHTML = (seconds < 10 ? '0' : '') + seconds;
   
+    //   let colors = document.getElementById("seconds").innerHTML;
+    //   for colors = ()  {
+    //       document.body.style.backgroundColor = (red);
+    //   }
+
+    let colors = ['Crimson','pink','coral','orange','CornflowerBlue','DarkViolet'];
+    (function changeColor(){
+      document.body.style.backgroundColor = colors[seconds++%colors.length];
+    })();
+
+    
       // Minutes
       let minutes = new Date().getMinutes();
       document.getElementById("minutes").innerHTML = (minutes < 10 ? '0' : '') + minutes;
@@ -12,4 +23,7 @@ window.onload = function() {
       let hours = new Date().getHours();
       document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
     });
-  }
+  };
+
+
+
